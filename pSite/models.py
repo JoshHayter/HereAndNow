a24d49714ceac44ad941d09d887c8530c96f2fbe
Josh Hayter <josh@joshhayter.co.uk>
2019-03-01 17:23:48 +0000

    -Added stock model

--- pSite/models.py
+++ pSite/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+def get_image_path(instance, filename):
+    return 'stock/{0}/{1}'.format(instance.id, filename)
+
+class Stock(models.Model):
+    title = models.CharField(max_length = 20)
+    artist = models.CharField(max_length = 20)
+    description = models.CharField(max_length = 100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.IntegerField()
+    image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
